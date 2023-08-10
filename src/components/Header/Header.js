eb9807b834f0d755e30748ead29c2ea4002aec52
vishwa <vishwa.goghari@kinship.co>
2023-08-10 19:04:39 -0400

    add welcome text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,19 +3,17 @@ import './Header.css';
 
 const Header = () => {
   return (
-    // <div className="header">
-    //   <div className="search-bar-outer-div">
-    //     <input type="text" placeholder="Find your role" />
-    //   </div>
-    //   <div className='inner-div'></div>
-    // </div>
     <div className="header">
-  <div className="inner-div"></div>
-  <div className="search-bar-outer-div">
-    <input type="text" placeholder="Find your role" />
-  </div>
-</div>
-
+      <div className="inner-div">
+        <h3 className='welcome'>Welcome</h3>
+        <p className='welcomeText'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className='welcomeTextTwo'>Interested? Learn more about our open roles below.</p>
+      </div>
+      <div className="search-bar-outer-div">
+        <input type="text" placeholder="Find your role" />
+      </div>
+    </div>
   );
 };
 
