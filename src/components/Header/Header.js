682c2066ead8ba563a0c25c88aed520f414d87be
vishwa <vishwa.goghari@kinship.co>
2023-08-10 18:38:53 -0400

    create an overlay position for the welcome section div

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import './Header.css'; // Make sure to import the CSS file
-
+import './Header.css';
 
 const Header = () => {
   return (
+    // <div className="header">
+    //   <div className="search-bar-outer-div">
+    //     <input type="text" placeholder="Find your role" />
+    //   </div>
+    //   <div className='inner-div'></div>
+    // </div>
     <div className="header">
-      <div className="search-bar-outer-div">
-        <input type="text" placeholder="Find your role" />
-      </div>
-      <div className='inner-div'>Inner div</div>
-    </div>
+  <div className="inner-div"></div>
+  <div className="search-bar-outer-div">
+    <input type="text" placeholder="Find your role" />
+  </div>
+</div>
+
   );
 };
 
