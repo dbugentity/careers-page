820480d232005459f14464a36c7a2640eb8d55cd
vishwa <vishwa.goghari@kinship.co>
2023-08-12 11:00:36 -0400

    notes and changes

--- src/App.js
+++ src/App.js
@@ -15,13 +15,28 @@ function App() {
      <LocomotiveScrollProvider options={options} containerRef={ref}>
       <main data-scroll-container ref={ref}>
         <section className="intro"
-      data-scroll //This attribute makes this section an independent scrollable container
-        data-scroll-speed="4"
+      data-scroll 
+      //** 
+
+
+      
+      /**
+       * 3rd part of the page here with sone floating content/elements after 1. locomotive text slide in,
+       *  2. parallax  
+       */
+
+      ////////////////
+        data-scroll-speed="9"
           data-scroll-section>
           <h1>This is the Introduction section</h1>
         </section>
         <section className="contents" data-scroll-section>
-          <h1>I Love React</h1>
+          <h1 data-scroll
+            data-scroll-direction="horizontal"
+            data-scroll-speed="2">I Love React</h1>
+          <h1  data-scroll
+            data-scroll-direction="vertical"
+            data-scroll-speed="9" >And my dog</h1>
         </section>
         <section className="footer" data-scroll-section>
           <h1>Let's end the application with this Footer</h1>
